e9e43935580921f45cc54d75311bddadaa2d8233
zeviberlin <zeviberlin@gmail.com>
2026-06-10 14:06:24 -0400

    better colors

--- include/colors.h
+++ include/colors.h
@@ -3,6 +3,8 @@
 
 #include <ncurses.h>
 
+
+
 #define GREEN 1
 #define DARK_GREEN 2
 #define RED 3
@@ -16,30 +18,6 @@
 /// can then use the space (' ') character with the
 /// background as a color instead of using a unicode
 /// character which requires a separate library
-void initColors() {
-  init_pair(GREEN, COLOR_BLACK, COLOR_GREEN);
-  init_pair(RED, COLOR_BLACK, COLOR_RED);
-  init_pair(CYAN, COLOR_BLACK, COLOR_CYAN);
-  init_pair(WHITE_CYAN, COLOR_WHITE, COLOR_CYAN);
-  init_pair(BLUE, COLOR_BLACK, COLOR_BLUE);
-
-  if (can_change_color()) {
-    // so sorry for all the magic numbers
-    init_color(COLOR_MAGENTA, 90, 224, 47); // change to dark green - scaled from (23,57,12)
-    init_color(COLOR_YELLOW, 690, 498, 59); // change to gold - scaled from (176,127,15)
-    init_color(200, 706, 412, 929); // create new violet color - scaled from (180,105,237)
-
-    init_pair(DARK_GREEN, COLOR_BLACK, COLOR_MAGENTA); // green or dark green depending
-    init_pair(VIOLET, COLOR_BLACK, 200); // green or dark green depending
-  } else {
-    init_pair(DARK_GREEN, COLOR_BLACK, COLOR_GREEN);  //  ^^^^^^^^
-    init_pair(VIOLET, COLOR_BLACK, COLOR_BLUE);  //  ^^^^^^^^
-  }
-  
-  /* its okay to change and then use COLOR_YELLOW because its not used elsewhere
-     changing a color changes it permanentely, like changing COLOR_GREEN just for 
-     the use of DARK_GREEN also changes the color for earlier-defined pairs */
-  init_pair(GOLD, COLOR_BLACK, COLOR_YELLOW); // yellow or gold depending
-}
+void initColors();
 
 #endif /* SNAKE_COLORS_H */
---
+++ src/colors.c
@@ -0,0 +1,39 @@
+#include "colors.h"
+
+#include <ncurses.h>
+
+void initColors() {
+  if (can_change_color()) { // ALL OF THOSE THIRD NUMBERS ARE MAGIC NUMBERS FOR 8-BIT COLORS
+    init_pair(GREEN, COLOR_BLACK, 22);
+
+    init_pair(DARK_GREEN, COLOR_BLACK, 5);
+
+    init_pair(RED, COLOR_BLACK, 160);
+    
+    init_pair(GOLD, COLOR_BLACK, 178);
+    
+    init_pair(CYAN, COLOR_BLACK, 159);
+    
+    init_pair(WHITE_CYAN, 240, 159); // gray - cyan
+    
+    init_pair(BLUE, COLOR_BLACK, 17);
+    
+    init_pair(VIOLET, COLOR_BLACK, 56);
+  } else {
+    init_pair(GREEN, COLOR_BLACK, COLOR_GREEN);
+
+    init_pair(DARK_GREEN, COLOR_BLACK, COLOR_GREEN);
+
+    init_pair(RED, COLOR_BLACK, COLOR_RED);
+
+    init_pair(GOLD, COLOR_BLACK, COLOR_YELLOW);
+
+    init_pair(CYAN, COLOR_BLACK, COLOR_CYAN);
+
+    init_pair(WHITE_CYAN, COLOR_WHITE, COLOR_CYAN);
+
+    init_pair(BLUE, COLOR_BLACK, COLOR_BLUE);
+
+    init_pair(VIOLET, COLOR_BLACK, COLOR_MAGENTA);
+  }
+}
